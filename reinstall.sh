95866ec204d1d5fa32f4a3717c6fe978787da126
Mariano Ronchi <marianoronchi@users.noreply.github.com>
2021-05-04 19:21:59 -0300

    Agrego Scripts de Rebuild
    Scripts varios

---
+++ reinstall.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#stop service
+sudo service thingsboard stop
+
+#drop db from CLI
+sudo -u postgres dropdb thingsboard
+
+#remove package
+sudo dpkg -r thingsboard
+
+#recreate database from CLI
+sudo -u postgres createdb thingsboard
+
+#install package
+sudo dpkg -i thingsboard/application/target/thingsboard.deb
+
+echo "siguientes pasos manuales:"
+
+echo "revisar pass db >>>  sudo nano /etc/thingsboard/conf/thingsboard.conf"
+
+echo "cambiar puerto a 5000  >>>  sudo nano /etc/thingsboard/conf/thingsboard.yml"
+
+echo "instalar datos demo >>> sudo /usr/share/thingsboard/bin/install/install.sh --loadDemo"
+
+echo "reinicia servicio >>> sudo systemctl daemon-reload | sudo service thingsboard start"
---
+++ reinstall_lite.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#stop service
+sudo service thingsboard stop
+
+#remove package
+sudo dpkg -r thingsboard
+
+#install package
+sudo dpkg -i thingsboard/application/target/thingsboard.deb
+
+#restart service
+sudo systemctl daemon-reload | sudo service thingsboard start
+
+echo "Done!"
